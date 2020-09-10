69a6a55a29cea8d653457813048415c2876c00e3
TimDunphy <timothy.dunphy@gmail.com>
2020-09-10 12:02:59 +0100

    Updated jsconfig and aliases for new motion dir.

--- webpack/aliases.js
+++ webpack/aliases.js
@@ -4,6 +4,7 @@ module.exports = {
     App: path.resolve(__dirname, '../src/app/'),
     Components: path.resolve(__dirname, '../src/app/components/'),
     Core: path.resolve(__dirname, '../src/core/'),
+    Motion: path.resolve(__dirname, '../src/motion/'),
     Pages: path.resolve(__dirname, '../src/app/pages/'),
     Routing: path.resolve(__dirname, '../src/core/routing/'),
     Util: path.resolve(__dirname, '../src/core/util')
